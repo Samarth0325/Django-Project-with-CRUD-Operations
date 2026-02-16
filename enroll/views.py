659a0271f70a663015b5a98d07ad0855deb0986c
Samarth0325 <samarthdeshmukhe3@gmail.com>
2026-02-16 17:10:59 +0530

    I can just adding a comment to learn about how we dont save the password in dtabase.

--- enroll/views.py
+++ enroll/views.py
@@ -9,7 +9,7 @@ def add_show(request):
         if fm.is_valid():
             nm = fm.cleaned_data['name']
             em = fm.cleaned_data['email']
-            pw = fm.cleaned_data['password']
+            pw = fm.cleaned_data['password'] #if we do not want to save password reduce this line and declared variable in second line.
             reg = User(name=nm, email=em, password=pw)
             reg.save()
     else:
